222a2e929a0860c2ce0d7efcfeee2879df7ecb85
Bogdan <bogdansolomich@gmail.com>
2024-04-23 18:23:50 +0300

    Создание приложения

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log(1);
+"use strict";
+
+const number0Offilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
+
+const personalMovieDB = {
+  count: number0Offilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const names = "Bogdan";
+
+const a = +prompt(`Один из последних просмотренных фильмов ${names}?`, ""),
+  b = +prompt("На сколько оцените его?", ""),
+  c = +prompt("Один из последних просмотренных фильмов?", ""),
+  d = +prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);

452d33a5b33f9f9e9393084c04548a24f255c8f4
Bogdan <bogdansolomich@gmail.com>
2024-05-04 17:03:30 +0300

    Ваш комментарий к изменениям

--- js/script.js
+++ js/script.js
@@ -1,23 +1,162 @@
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит -
+возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как
+str.length - и получить её длину)
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше -
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
 "use strict";
 
-const number0Offilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  }
+}
+
+start();
 
 const personalMovieDB = {
-  count: number0Offilms,
+  count: numberOfFilms,
   movies: {},
   actors: {},
   genres: [],
   privat: false,
 };
 
-const names = "Bogdan";
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", "");
 
-const a = +prompt(`Один из последних просмотренных фильмов ${names}?`, ""),
-  b = +prompt("На сколько оцените его?", ""),
-  c = +prompt("Один из последних просмотренных фильмов?", ""),
-  d = +prompt("На сколько оцените его?", "");
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log("done");
+    } else {
+      console.log("error");
+      i--;
+    }
+  }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+rememberMyFilms();
 
-console.log(personalMovieDB);
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+  } else {
+    console.log("Произошла ошибка");
+  }
+}
+
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+  for (let i = 0; i <= 3; i++) {
+    const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+    personalMovieDB.genres[i - 1] = genre;
+  }
+}
+writeYourGenres();
+
+// function thirdTask() {
+//   for (let i = 2; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// function fifthTask() {
+//   const dataarr = [];
+//   for (let i = 5; i <= 10; i++) {
+//     dataarr[i];
+//   }
+// }
+
+//  Цикл, который нужно переписать:
+
+// for (let i = 2; i <= 16; i++) {
+//   if (i % 2 === 0) {
+//     continue;
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// function fifthTask() {
+//   let i = 2;
+//   while (i < 17) {
+//     if (i % 2 === 0) {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// const derty = 24;
+// const ferys = 35;
+
+// function sadertyuw(nums, numcurrents) {
+//   console.log(nums * numcurrents);
+// }
+
+// sadertyuw(300, derty);
+// sadertyuw(200, ferys);
+
+// function makeTransaction(quantity, pricePerDroid) {
+//   const totalPrice = quantity * pricePerDroid;
+//   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+// }
+
+// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+// function getShippingMessage(country, price, deliveryFee) {
+//   const totalPrice = price + deliveryFee;
+//   return `Shipping to ${country} will cost ${totalPrice} credits`;
+// }
+
+// console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
+// console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
+// console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+// function getElementWidth(content, padding, border) {
+//   const contentWidth = parseFloat(content);
+//   const paddingWidth = parseFloat(padding);
+//   const borderWidth = parseFloat(border);
+
+//   const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+//   return totalWidth;
+// }
+
+// console.log(getElementWidth("50px", "8px", "4px")); // 74
+// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+// console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+// const testic = "Kiev Bogdan";
+
+// console.log(testic.indexOf("g"));
